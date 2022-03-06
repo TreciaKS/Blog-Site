0b30bfdce992dcda24d33f6e35abbcb73cd5a4a4
TreciaKS <tksekgalo@gmail.com>
2022-03-06 15:12:42 +0000

    Created and styled Navbar

--- blog-site/src/App.js
+++ blog-site/src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline text-red-300">
-      Hello world!
-    </h1>
+      <NavBar />
     </div>
   );
 }
