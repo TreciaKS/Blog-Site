ea620e2bd8c09103214429297732f6fed297f654
TreciaKS <tksekgalo@gmail.com>
2022-03-09 10:00:44 +0000

    Used unsplash pictures to since I'm terrible with file directory

--- blog-site/src/App.js
+++ blog-site/src/App.js
@@ -6,7 +6,11 @@ function App() {
   return (
     <div className="App">
       <NavBar />
-      <Cards />
+      <Cards 
+      image= {"https://images.unsplash.com/photo-1544856890-7fdbecf97726?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGRpc2h8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"}
+      title = {"This is my first time"}
+      description = {"Building a react app is reallly nice, for now"}
+      />
     </div>
   );
 }
